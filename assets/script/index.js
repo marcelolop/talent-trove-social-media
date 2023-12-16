0fd4f447646f853e4dccf1e5bbd798483ffac11c
marcelolop <marcelolopfern@hotmail.com>
2023-12-15 21:32:09 -0600

    Some small changes, and fixes

--- assets/script/index.js
+++ assets/script/index.js
@@ -13,29 +13,6 @@ import {
 } from "./utils/general.js";
 
 /*
-Directions and requirements
-• The application consists of two pages: one for user login (index) and one for the main page 
-(home). Ensure that the login section occupies an entire page rather than appearing as a
-dialog.
-• The home page should have a header, a footer, and a 3-column layout. Base your design on 
-one or more examples provided. Responsive design is optional.
-• Store login information (username and password) in localStorage. Upon clicking the login 
-button, the system will access this stored data, comparing it with the provided credentials. If 
-the comparison is successful, your code will redirect the user to the homepage. Otherwise, 
-display an error message ('Incorrect username or password').
-• Remember: you need to provide the credentials your application will store, so I can test it. 
-Add this information to your comment on MyLeaning.
-• You, as the main user, may add your own picture and data manually.
-• Include other users (at least 10), such as your connections or 'people you may know,' retrieved 
-from the Random User API. Display only their profile pictures, full names, and cities.
-* Check examples online. Avoid creating things that make no sense.
-* Maintain consistency throughout the project's pages, ensuring uniformity in colours, fonts, 
-* margins, and other design elements.
-* Host your application on GitHub and provide a link to the repository.
-* Again, check the examples provided (and others) to create pages that look professional
-*/
-
-/*
 !_______________________
 !  User Login Page     |
 !_______________________
